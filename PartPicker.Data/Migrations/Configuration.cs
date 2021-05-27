edf55be2f496d7928718b4899d5b2a902d85e8a5
Trejon-Hughes <trejonhughes15@gmail.com>
2021-05-27 13:41:10 -0400

    Initial Commit

--- PartPicker.Data/Migrations/Configuration.cs
+++ PartPicker.Data/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace PartPicker.Data.Migrations
+﻿namespace PartPicker.Data.Migrations
 {
     using System;
     using System.Data.Entity;
@@ -9,24 +9,15 @@ namespace PartPicker.Data.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
-            ContextKey = "PartPicker.Data.ApplicationDbContext";
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(PartPicker.Data.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
         }
     }
 }
